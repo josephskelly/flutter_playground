ca65d98ce68b710963162ec00adbb03a522c0ace
Joseph Kelly <jskelly84@gmail.com>
2020-11-14 19:11:41 -0800

    Adds Navigator 2.0 objects. Reorganizes some code to separate files.

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,137 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_playground/Counter.dart';
-import 'package:flutter_playground/RandomWords.dart';
+import 'package:flutter_playground/HomePage.dart';
 
-void main() => runApp(CounterApp());
+void main() => runApp(Playground());
 
-///Generates random names by compounding two words.
-class RandomWordsApp extends StatelessWidget {
+class Project {
+  final String title;
+  final String description;
+
+  Project(this.title, this.description);
+}
+
+class Playground extends StatefulWidget {
+  @override
+  _PlaygroundState createState() => _PlaygroundState();
+}
+
+class _PlaygroundState extends State<Playground> {
+  void initState() {
+    super.initState();
+  }
+
+  Project _selectedProject;
+  bool show404 = false;
+  List<Project> projects = [
+    Project('Startup Name Generator',
+        'Creates random names by combining random words'),
+    Project('Counter', 'Keeps track of how many times a button was pressed'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Startup Name Generator',
+      title: 'Playground',
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: RandomWords(),
+      home: Navigator(
+        pages: [
+          MaterialPage(
+            key: ValueKey('HomePage'),
+            child: HomePage(
+              projects: projects,
+              onTapped: _handleProjectTapped,
+            ),
+          ),
+          if (show404)
+            MaterialPage(
+              key: ValueKey('UnknownPage'),
+              child: UnknownScreen(),
+            )
+          else if (_selectedProject != null)
+            MaterialPage(
+              key: ValueKey(_selectedProject),
+              child: ProjectDetailsScreen(project: _selectedProject),
+            )
+        ],
+        onPopPage: (route, result) {
+          if (!route.didPop(result)) {
+            return false;
+          }
+
+          // Update the list of pages by setting _selectedBook to null
+          setState(() {
+            _selectedProject = null;
+          });
+
+          return true;
+        },
+      ),
     );
+  }
+
+  void _handleProjectTapped(Project project) {
+    setState(() {
+      _selectedProject = project;
+    });
   }
 }
 
-///A simple button that keeps track of how many times it has been pressed.
-class CounterApp extends StatelessWidget {
+class UnknownScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      title: 'Cupertino Counter',
-      home: Counter(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Unknown Project'),
+      ),
+      body: Text('An error occurred.'),
     );
   }
 }
 
+class ProjectDetailsScreen extends StatelessWidget {
+  final Project project;
+  ProjectDetailsScreen({
+    @required this.project,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(project.title),
+      ),
+      body: Text(project.description),
+    );
+  }
+}
+
+// ///Generates random names by compounding two words.
+// class RandomWordsApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Startup Name Generator',
+//       theme: ThemeData(
+//         primaryColor: Colors.white,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
+//
+// ///A simple button that keeps track of how many times it has been pressed.
+// class CounterApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoApp(
+//       title: 'Cupertino Counter',
+//       home: Counter(),
+//     );
+//   }
+// }
+//
