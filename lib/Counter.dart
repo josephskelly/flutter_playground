82cb3b213a04ca7feed95b5544a26e4e0d188b43
Joseph Kelly <jskelly84@gmail.com>
2021-03-22 13:41:14 -0700

    Adds a simple animation to counter page

--- lib/Counter.dart
+++ lib/Counter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
 
 // TODO: Add something fun for counter challenge.
 
@@ -38,9 +40,59 @@ class _CounterState extends State<Counter> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       // floatingActionButtonAnimator: ,
       body: Center(
-        child: Text('Count: $_counter'),
+        child: Column(
+          children: [
+            Text('Count: $_counter'),
+            Container(
+              child: MyRiveAnimation(),
+              height: 200,
+              width: 200,
+            ),
+          ],
+        ),
         heightFactor: 37,
       ),
     );
   }
 }
+
+class MyRiveAnimation extends StatefulWidget {
+  @override
+  _MyRiveAnimationState createState() => _MyRiveAnimationState();
+}
+
+class _MyRiveAnimationState extends State<MyRiveAnimation> {
+  final riveFileName = 'assets/crap_animation.riv';
+  Artboard _artboard;
+
+  @override
+  void initState() {
+    _loadRiveFile();
+    super.initState();
+  }
+
+  void _loadRiveFile() async {
+    final bytes = await rootBundle.load(riveFileName);
+    final file = RiveFile();
+
+    if (file.import(bytes)) {
+      // Select an animation by its name
+      setState(() => _artboard = file.mainArtboard
+        ..addController(
+          SimpleAnimation('Animation 1'),
+        )
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _artboard != null
+      ? Rive(
+          artboard: _artboard,
+          fit: BoxFit.cover,
+        )
+      : Container();
+  }
+}
+
--- lib/Router.gr.dart
+++ lib/Router.gr.dart
@@ -5,16 +5,18 @@
 // **************************************************************************
 
 import 'package:auto_route/auto_route.dart' as _i1;
-import 'ProjectListScreen.dart' as _i2;
-import 'RandomWords.dart' as _i3;
-import 'Counter.dart' as _i4;
-import 'SignIn.dart' as _i5;
-import 'WelcomePage.dart' as _i6;
-import 'animated_transition/animated_transition_home.dart' as _i7;
-import 'animated_transition/animated_transition_destination.dart' as _i8;
-import 'SpringSimulation.dart' as _i9;
 import 'package:flutter/material.dart' as _i10;
-import 'Project.dart' as _i11;
+import 'package:flutter_playground/animated_transition/animated_transition_destination.dart'
+    as _i8;
+import 'package:flutter_playground/animated_transition/animated_transition_home.dart'
+    as _i7;
+import 'package:flutter_playground/Counter.dart' as _i4;
+import 'package:flutter_playground/Project.dart' as _i11;
+import 'package:flutter_playground/ProjectListScreen.dart' as _i2;
+import 'package:flutter_playground/RandomWords.dart' as _i3;
+import 'package:flutter_playground/SignIn.dart' as _i5;
+import 'package:flutter_playground/SpringSimulation.dart' as _i9;
+import 'package:flutter_playground/WelcomePage.dart' as _i6;
 
 class AppRouter extends _i1.RootStackRouter {
   AppRouter();
@@ -22,93 +24,123 @@ class AppRouter extends _i1.RootStackRouter {
   @override
   final Map<String, _i1.PageFactory> pagesMap = {
     ProjectListScreenRoute.name: (entry) {
-      var route = entry.routeData.as<ProjectListScreenRoute>();
+      var args = entry.routeData.argsAs<ProjectListScreenRouteArgs>(
+          orElse: () => ProjectListScreenRouteArgs());
       return _i1.AdaptivePage(
-          entry: entry, child: _i2.ProjectListScreen(onTapped: route.onTapped));
+          entry: entry,
+          child: _i2.ProjectListScreen(onTapped: args.onTapped),
+          maintainState: true,
+          fullscreenDialog: false);
     },
     RandomWordsRoute.name: (entry) {
-      return _i1.AdaptivePage(entry: entry, child: _i3.RandomWords());
+      return _i1.AdaptivePage(
+          entry: entry,
+          child: _i3.RandomWords(),
+          maintainState: true,
+          fullscreenDialog: false);
     },
     FavoritesRoute.name: (entry) {
-      return _i1.AdaptivePage(entry: entry, child: _i3.Favorites());
+      return _i1.AdaptivePage(
+          entry: entry,
+          child: _i3.Favorites(),
+          maintainState: true,
+          fullscreenDialog: false);
     },
     CounterRoute.name: (entry) {
-      return _i1.AdaptivePage(entry: entry, child: _i4.Counter());
+      return _i1.AdaptivePage(
+          entry: entry,
+          child: _i4.Counter(),
+          maintainState: true,
+          fullscreenDialog: false);
     },
     SignInRoute.name: (entry) {
-      return _i1.AdaptivePage(entry: entry, child: _i5.SignIn());
+      return _i1.AdaptivePage(
+          entry: entry,
+          child: _i5.SignIn(),
+          maintainState: true,
+          fullscreenDialog: false);
     },
     WelcomePageRoute.name: (entry) {
-      return _i1.AdaptivePage(entry: entry, child: _i6.WelcomePage());
+      return _i1.AdaptivePage(
+          entry: entry,
+          child: _i6.WelcomePage(),
+          maintainState: true,
+          fullscreenDialog: false);
     },
     AnimatedTransitionHomeRoute.name: (entry) {
       return _i1.AdaptivePage(
-          entry: entry, child: _i7.AnimatedTransitionHome());
+          entry: entry,
+          child: _i7.AnimatedTransitionHome(),
+          maintainState: true,
+          fullscreenDialog: false);
     },
     AnimatedTransitionDestinationRoute.name: (entry) {
       return _i1.CustomPage(
           entry: entry,
           child: _i8.AnimatedTransitionDestination(),
-          transitionsBuilder: _i1.TransitionsBuilders.fadeIn);
+          maintainState: true,
+          fullscreenDialog: false,
+          transitionsBuilder: _i1.TransitionsBuilders.fadeIn,
+          opaque: true,
+          barrierDismissible: false);
     },
     SpringSimulationRoute.name: (entry) {
-      return _i1.AdaptivePage(entry: entry, child: _i9.SpringSimulation());
+      return _i1.AdaptivePage(
+          entry: entry,
+          child: _i9.SpringSimulation(),
+          maintainState: true,
+          fullscreenDialog: false);
     }
   };
 
   @override
   List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig<ProjectListScreenRoute>(ProjectListScreenRoute.name,
-            path: '/',
-            routeBuilder: (match) => ProjectListScreenRoute.fromMatch(match)),
-        _i1.RouteConfig<RandomWordsRoute>(RandomWordsRoute.name,
-            path: '/random-words',
-            routeBuilder: (match) => RandomWordsRoute.fromMatch(match)),
-        _i1.RouteConfig<FavoritesRoute>(FavoritesRoute.name,
+        _i1.RouteConfig(ProjectListScreenRoute.name,
+            path: '/', fullMatch: false, usesTabsRouter: false),
+        _i1.RouteConfig(RandomWordsRoute.name,
+            path: '/random-words', fullMatch: false, usesTabsRouter: false),
+        _i1.RouteConfig(FavoritesRoute.name,
             path: '/random-words/favorites',
-            routeBuilder: (match) => FavoritesRoute.fromMatch(match)),
-        _i1.RouteConfig<CounterRoute>(CounterRoute.name,
-            path: '/Counter',
-            routeBuilder: (match) => CounterRoute.fromMatch(match)),
-        _i1.RouteConfig<SignInRoute>(SignInRoute.name,
-            path: '/sign-in',
-            routeBuilder: (match) => SignInRoute.fromMatch(match)),
-        _i1.RouteConfig<WelcomePageRoute>(WelcomePageRoute.name,
-            path: '/sign-in/welcome',
-            routeBuilder: (match) => WelcomePageRoute.fromMatch(match)),
-        _i1.RouteConfig<AnimatedTransitionHomeRoute>(
-            AnimatedTransitionHomeRoute.name,
+            fullMatch: false,
+            usesTabsRouter: false),
+        _i1.RouteConfig(CounterRoute.name,
+            path: '/Counter', fullMatch: false, usesTabsRouter: false),
+        _i1.RouteConfig(SignInRoute.name,
+            path: '/sign-in', fullMatch: false, usesTabsRouter: false),
+        _i1.RouteConfig(WelcomePageRoute.name,
+            path: '/sign-in/welcome', fullMatch: false, usesTabsRouter: false),
+        _i1.RouteConfig(AnimatedTransitionHomeRoute.name,
             path: '/animated-transition-home',
-            routeBuilder: (match) =>
-                AnimatedTransitionHomeRoute.fromMatch(match)),
-        _i1.RouteConfig<AnimatedTransitionDestinationRoute>(
-            AnimatedTransitionDestinationRoute.name,
+            fullMatch: false,
+            usesTabsRouter: false),
+        _i1.RouteConfig(AnimatedTransitionDestinationRoute.name,
             path: '/animated-transition-destination',
-            routeBuilder: (match) =>
-                AnimatedTransitionDestinationRoute.fromMatch(match)),
-        _i1.RouteConfig<SpringSimulationRoute>(SpringSimulationRoute.name,
-            path: '/spring-simulation',
-            routeBuilder: (match) => SpringSimulationRoute.fromMatch(match))
+            fullMatch: false,
+            usesTabsRouter: false),
+        _i1.RouteConfig(SpringSimulationRoute.name,
+            path: '/spring-simulation', fullMatch: false, usesTabsRouter: false)
       ];
 }
 
-class ProjectListScreenRoute extends _i1.PageRouteInfo {
+class ProjectListScreenRoute
+    extends _i1.PageRouteInfo<ProjectListScreenRouteArgs> {
   ProjectListScreenRoute({@_i10.required this.onTapped})
-      : super(name, path: '/');
-
-  ProjectListScreenRoute.fromMatch(_i1.RouteMatch match)
-      : onTapped = null,
-        super.fromMatch(match);
+      : super(name,
+            path: '/', args: ProjectListScreenRouteArgs(onTapped: onTapped));
 
   final void Function(_i11.Project) onTapped;
 
   static const String name = 'ProjectListScreenRoute';
 }
 
+class ProjectListScreenRouteArgs {
+  const ProjectListScreenRouteArgs({@_i10.required this.onTapped});
+
+  final void Function(_i11.Project) onTapped;
+}
+
 class RandomWordsRoute extends _i1.PageRouteInfo {
   const RandomWordsRoute() : super(name, path: '/random-words');
-
-  RandomWordsRoute.fromMatch(_i1.RouteMatch match) : super.fromMatch(match);
 
   static const String name = 'RandomWordsRoute';
 }
@@ -116,15 +148,11 @@ class RandomWordsRoute extends _i1.PageRouteInfo {
 class FavoritesRoute extends _i1.PageRouteInfo {
   const FavoritesRoute() : super(name, path: '/random-words/favorites');
 
-  FavoritesRoute.fromMatch(_i1.RouteMatch match) : super.fromMatch(match);
-
   static const String name = 'FavoritesRoute';
 }
 
 class CounterRoute extends _i1.PageRouteInfo {
   const CounterRoute() : super(name, path: '/Counter');
-
-  CounterRoute.fromMatch(_i1.RouteMatch match) : super.fromMatch(match);
 
   static const String name = 'CounterRoute';
 }
@@ -132,15 +160,11 @@ class CounterRoute extends _i1.PageRouteInfo {
 class SignInRoute extends _i1.PageRouteInfo {
   const SignInRoute() : super(name, path: '/sign-in');
 
-  SignInRoute.fromMatch(_i1.RouteMatch match) : super.fromMatch(match);
-
   static const String name = 'SignInRoute';
 }
 
 class WelcomePageRoute extends _i1.PageRouteInfo {
   const WelcomePageRoute() : super(name, path: '/sign-in/welcome');
-
-  WelcomePageRoute.fromMatch(_i1.RouteMatch match) : super.fromMatch(match);
 
   static const String name = 'WelcomePageRoute';
 }
@@ -149,9 +173,6 @@ class AnimatedTransitionHomeRoute extends _i1.PageRouteInfo {
   const AnimatedTransitionHomeRoute()
       : super(name, path: '/animated-transition-home');
 
-  AnimatedTransitionHomeRoute.fromMatch(_i1.RouteMatch match)
-      : super.fromMatch(match);
-
   static const String name = 'AnimatedTransitionHomeRoute';
 }
 
@@ -159,17 +180,11 @@ class AnimatedTransitionDestinationRoute extends _i1.PageRouteInfo {
   const AnimatedTransitionDestinationRoute()
       : super(name, path: '/animated-transition-destination');
 
-  AnimatedTransitionDestinationRoute.fromMatch(_i1.RouteMatch match)
-      : super.fromMatch(match);
-
   static const String name = 'AnimatedTransitionDestinationRoute';
 }
 
 class SpringSimulationRoute extends _i1.PageRouteInfo {
   const SpringSimulationRoute() : super(name, path: '/spring-simulation');
-
-  SpringSimulationRoute.fromMatch(_i1.RouteMatch match)
-      : super.fromMatch(match);
 
   static const String name = 'SpringSimulationRoute';
 }
